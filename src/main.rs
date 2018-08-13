10d2efef05d60c0332f463e9cb61ab10422b17ca
Dawson Israel <dawfre@amazon.com>
2018-08-13 15:41:26 -0300

    Add health endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate actix_web;
+
+use std::env;
+use actix_web::{server, App, HttpRequest, Responder};
+
+fn health(_: &HttpRequest) -> impl Responder {
+    "Hello World!"
+}
+
 fn main() {
-    println!("Hello, world!");
+    let port = env::var("PORT").unwrap_or("8000".to_string());
+
+    server::new(|| {
+        App::new().resource("/health", |r| r.f(health))
+    })
+    .bind(format!("0.0.0.0:{}", &port).as_str())
+    .expect(format!("Can not bind to port {}", port).as_str())
+    .run();
 }
