886fbd5a2da54e540c89f18036b55cfcbe204211
Dawson Israel <dawfre@amazon.com>
2018-08-14 17:29:57 -0300

    Add Log

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,33 @@
+extern crate actix;
 extern crate actix_web;
+extern crate env_logger;
+extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
+extern crate json;
 
 mod controllers;
 
+use actix::System;
+use actix_web::{middleware, server, App};
 use controllers::*;
 use std::env;
-use actix_web::{server, App};
-
 
 fn main() {
+    env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
     let port = env::var("PORT").unwrap_or("8000".to_string());
+    let sys = System::new("Wormhole");
 
     server::new(|| {
-        App::new().resource("/health", |r| r.f(health))
-    })
-    .bind(format!("0.0.0.0:{}", &port).as_str())
-    .expect(format!("Can not bind to port {}", port).as_str())
-    .run();
+        App::new()
+            .middleware(middleware::Logger::default())
+            .resource("/health", |r| r.f(health))
+    }).bind(format!("0.0.0.0:{}", &port).as_str())
+        .unwrap()
+        .shutdown_timeout(1)
+        .start();
+
+    let _ = sys.run();
 }
