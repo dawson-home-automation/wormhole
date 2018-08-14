b39ebf0cc2d715176049b7115736661f2ba72249
Dawson Israel <dawfre@amazon.com>
2018-08-14 19:22:14 -0300

    Add mqtt publish

--- src/controllers/publisher.rs
+++ src/controllers/publisher.rs
@@ -1,13 +1,48 @@
-use actix_web::{HttpResponse, Responder, Json};
+extern crate rumqtt;
+
+use self::rumqtt::{MqttClient, MqttOptions, QoS};
+use actix_web::{HttpResponse, Json, Responder};
 
 #[derive(Debug, Deserialize)]
 pub struct Request {
     topic: String,
-    payload: String
+    payload: String,
 }
 
-pub fn publish(payload: Json<Request>) -> impl Responder {
-    println!("{:?}", payload);
+impl Request {
+
+    pub fn get_topic(&self) -> &String {
+        &self.topic
+    }
+
+    pub fn get_payload(&self) -> &String {
+        &self.payload
+    }
+
+}
+
+pub fn publish(request: Json<Request>) -> impl Responder {
+    println!("{:?}", request);
+
+    let mut mq_client = get_mqtt_client();
+
+    mq_client
+        .publish(
+            request.get_topic().as_str(),
+            QoS::Level1,
+            request.get_payload().clone().into_bytes(),
+        )
+        .expect("Publish failure");
 
     HttpResponse::Ok()
+}
+
+fn get_mqtt_client() -> MqttClient {
+    let client_options = MqttOptions::new()
+        .set_keep_alive(5)
+        .set_reconnect(3)
+        .set_client_id("wormhole")
+        .set_broker("test.mosquitto.org:1883");
+
+    MqttClient::start(client_options, None).unwrap()
 }
